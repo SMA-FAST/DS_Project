862ad2c9f1ce0ca8b02cdd1737938a9798e817ce
SMA-FAST <i230022@isb.nu.edu.pk>
2024-12-07 19:45:25 +0500

    Added basic classes for data of roads, intersection and vehicle

--- ObjectClasses.cpp
+++ ObjectClasses.cpp
@@ -1,1 +1,18 @@
 //Road, Vehicle, etc classes
+#include<string>
+using namespace std;
+class Road
+{
+    char from_intersection;
+    char to_intersection;
+    string status;
+};
+class Intersection
+{
+    char name;
+};
+class Vehicle
+{
+    string id;
+    string priority;
+};
